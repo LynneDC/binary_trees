09b490e03eb84f1aea9d840f27696f5e4ac2b48c
opxzy22 <aopeyem84@gmail.com>
2023-08-29 15:59:48 -0700

    a function that deletes an entire binary tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_insert_right- func inserts node as right-child of another node
+ *binary_tree_insert_right- function inserts node as right-child of another node
  *@parent: pointer to the node to insert the left-child in
  *@value: the value to store in the new node
  * Return: new or NULL if fails
