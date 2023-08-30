ccea63aaf00bc3f38ea7c8e5944287d1750e0fbe
LynneDC <roselinedc1308@gmail.com>
2023-08-30 16:59:59 +0200

    update

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,17 +8,17 @@
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+binary_tree_t *parent = node->parent;
 if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 {
 return (NULL);
 }
 
-binary_tree_t *parent = node->parent;
-
 
 if (parent->parent->left == node->parent)
+{
 return (parent->parent->right);
-else
+}
 return (parent->parent->left);
 }
 
