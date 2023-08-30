80531313e414f920954be2295da2fad63f2cbde4
LynneDC <roselinedc1308@gmail.com>
2023-08-30 18:43:45 +0200

    ls

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,6 +11,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 binary_tree_t *parent = node->parent;
 if (node == NULL || node->parent == NULL 
+|| node->parent->parent == NULL
 || node->parent->parent->left == NULL
 || node->parent->parent->right == NULL)
 return NULL;
