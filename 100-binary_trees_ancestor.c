e0f3cf7d2755374590b56201344e82bf5e1c394f
LynneDC <roselinedc1308@gmail.com>
2023-08-30 17:53:53 +0200

    new

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,7 +7,8 @@
 *
 * Return: A pointer to the lca node, or NULL if not found.
 */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
 {
 if (first == NULL || second == NULL)
 return (NULL);
