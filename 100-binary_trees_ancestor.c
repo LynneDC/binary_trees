85b062bb7f4076fe64218efc81ade33c63301761
LynneDC <roselinedc1308@gmail.com>
2023-08-30 16:49:30 +0200

    update

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+* binary_trees_ancestor - Finds the lca of two nodes.
+* @first: A pointer to the first node.
+* @second: A pointer to the second node.
+*
+* Return: A pointer to the lca node, or NULL if not found.
+*/
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+if (first == NULL || second == NULL)
+return (NULL);
+
+while (first != NULL)
+{
+binary_tree_t *temp = (binary_tree_t *)second;
+
+while (temp != NULL)
+{
+if (first == temp)
+return ((binary_tree_t *)first);
+temp = temp->parent;
+}
+first = first->parent;
+}
+
+return (NULL);
+}
