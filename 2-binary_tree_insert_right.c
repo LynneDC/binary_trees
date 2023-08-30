929af6d437106e4bb6532ceee16242a7d3b2eb07
LynneDC <roselinedc1308@gmail.com>
2023-08-30 07:57:38 +0200

    inserts right child

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_insert_right- function inserts node as right-child of another node
+ *binary_tree_insert_right- fun inserts node as right-child
  *@parent: pointer to the node to insert the left-child in
  *@value: the value to store in the new node
  * Return: new or NULL if fails
