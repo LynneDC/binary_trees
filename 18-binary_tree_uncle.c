1163c2cd73bfe41947154bc73f59fce3e639b120
LynneDC <roselinedc1308@gmail.com>
2023-08-31 12:19:37 +0200

    find uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,15 +9,15 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-binary_tree_t *parent = node->parent;
+
 if (node == NULL || node->parent == NULL
 || node->parent->parent == NULL
 || node->parent->parent->left == NULL
 || node->parent->parent->right == NULL)
 return (NULL);
 
-if (parent->parent->left == parent)
-return (parent->parent->right);
+if (node->parent->parent->left == node->parent)
+return (node->parent->parent->right);
 else
-return (parent->parent->left);
+return (node->parent->parent->left);
 }
